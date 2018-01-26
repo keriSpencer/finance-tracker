82a666a4f87914d144de3a6bfc0cc70adc710aa6
Keri Spencer <keri.spencer@redvector.com>
2018-01-26 10:52:02 -0500

    Refactor StocksController

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,13 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render 'stocks/search' }
-        end
-      else
-        respond_to do |format|
-          flash.now[:danger] = "You have entered an incorrect symbol"
-          format.js { render 'stocks/search' }
-        end
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty string"
     else
-      respond_to do |format|
-        flash.now[:danger] = "You have entered an empty string"
-        format.js { render 'stocks/search' }
-      end
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+    end
+    respond_to do |format|
+      format.js { render 'stocks/search' }
     end
   end
 
